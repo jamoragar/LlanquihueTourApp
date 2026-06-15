3befc73acb3bb7035e0e6addb49e59c5f423dfc1
jamoragar <javier.moragarojas@gmail.com>
2026-06-15 03:00:02 -0400

    F

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -10,13 +10,13 @@ public class Main {
     public static void main(String[] args) {
 
         // Ruta del archivo de texto
-        String rutaArchivo = "resources/tours.txt";
+        String archivoTours = "resources/tours.txt";
 
         // Se crea un objeto de la clase GestorDatos
         GestorDatos gestor = new GestorDatos();
 
         // Se carga la lista de tours desde el archivo
-        ArrayList<Tour> tours = gestor.cargarTours(rutaArchivo);
+        ArrayList<Tour> tours = gestor.cargarTours(archivoTours);
 
         // Mostrar todos los tours cargados
         System.out.println("===== CATALOGO COMPLETO DE LLANQUIHUE TOUR =====");
